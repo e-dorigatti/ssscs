34d3c1215c621d9ea727f12ed19bfefc517d96fd
emilio <emilio.dorigatti@gmail.com>
2015-09-25 11:31:55 +0200

    doc and tweaking

--- make-graph.py
+++ make-graph.py
@@ -1,10 +1,22 @@
 """
 Generates a random directed graph; The number of outbound edges per node is
 distributed normally.
+
 The graph is saved in a file with the following format:
   <number of nodes>\n
   <number of edges>\n
   one row for each edge u-->v: u<space>v
+
+The total number of processes used is n worker + 1 writer + 1 master
+best performances can be achieved by setting n to cpu count - 1 because
+the writer and the master don't have significan cpu loads so they can
+share the same cpu
+
+The size of the nodes queue, with which the master passes work packs
+to the workers, does affect the performances as well. It trades off
+processing speed for ctrl-c responsiveness, as the workers have to
+process all the packs in the queue before encountering the stop
+signal.
 """
 
 import click
@@ -15,6 +27,7 @@ import sys
 
 
 def worker_process(in_queue, out_queue, num_nodes):
+    # ignore keyboard interrupts, the master will take care of them
     signal.signal(signal.SIGINT, signal.SIG_IGN)
 
     pack = in_queue.get()
@@ -27,10 +40,12 @@ def worker_process(in_queue, out_queue, num_nodes):
 
 
 def writer_process(in_queue, fp):
+    # ignore keyboard interrupts, the master will take care of them
     signal.signal(signal.SIGINT, signal.SIG_IGN)
-    anim = ['\\', '|', '/', '-']
 
+    anim = ['\\', '|', '/', '-']
     i = 0
+
     row = in_queue.get()
     while row is not None:
         fp.write(row)
@@ -51,9 +66,10 @@ def writer_process(in_queue, fp):
               help='Average value of outbout edges per node')
 @click.option('--degree-stdev', '-D', default=1,
               help='Standard deviation of outbound edge count per node')
-@click.option('--processes', '-p', default=mp.cpu_count(),
-              help='How many processes to use to generate the graph')
-def main(out_file, nodes, degree_avg, degree_stdev, processes):
+@click.option('--processes', '-p', default=mp.cpu_count()-1,
+              help='How many worker processes to use to generate the graph')
+@click.option('--queue-size', '-q', default=500, help='Size of the nodes queue.')
+def main(out_file, nodes, degree_avg, degree_stdev, processes, queue_size):
     """
     Generates a random directed graph. The number of outbound edges per node
     is distributed normally.
@@ -63,10 +79,9 @@ def main(out_file, nodes, degree_avg, degree_stdev, processes):
              for _ in range(nodes)]
     print 'Graph will have %d edges' % sum(edges)
     out_file.write('%d\n%d\n' % (nodes, sum(edges)))
-    out_file.flush()
+    out_file.flush()  # do not buffer, actually write this to file
 
-    # using a small queue so that interrupt signal are received quickly
-    in_queue, out_queue = mp.Queue(10), mp.Queue()
+    in_queue, out_queue = mp.Queue(queue_size), mp.Queue()
     processes = [mp.Process(target=worker_process, args=(in_queue, out_queue, nodes))
                  for _ in range(processes)]
     processes.append(mp.Process(target=writer_process, args=(out_queue, out_file)))
@@ -76,7 +91,7 @@ def main(out_file, nodes, degree_avg, degree_stdev, processes):
         for node, num_edges in enumerate(edges):
             in_queue.put((node, num_edges))
     except KeyboardInterrupt:
-        pass
+        print 'Workers will stop after they process the next %d nodes' % queue_size
 
     [in_queue.put(None) for _ in processes]
     [p.join() for p in processes[:-1]]
